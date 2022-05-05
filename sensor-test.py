da00dc8090a8a3730bad7e11101e10e1f7d30574
DanielKuebi <daniflug95@live.at>
2022-05-05 12:23:46 +0200

    folder cleanup and bugfix

--- sensor-test.py
+++ sensor-test.py
@@ -12,18 +12,16 @@ GPIO.setup(27, GPIO.OUT)
 
 if __name__ == "__main__":
     print("<<< Test sensor assembly. >>>")
-    print("Ventilator test...")
 
     # test ventilator
     GPIO.output(27, True)
-    time.sleep(1)
+    time.sleep(5)
     GPIO.output(27, False)
-
-    print("Sensor test...")
 
     sensors = [EcSensor('/dev/ttyS0'), EcSensor('/dev/ttyAMA1'), EcSensor('/dev/ttyAMA2')]
 
     for sensor in sensors:
+
         [gas_concentration, temperature, humidity] = sensor.read()
 
         print('\n|>------------------------------')
